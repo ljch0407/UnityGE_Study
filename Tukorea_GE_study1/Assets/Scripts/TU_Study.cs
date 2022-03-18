35371a02794002231cb7c8774ed35945f2714c36
ljch0407 <ljch0407@gmail.com>
2022-03-18 10:58:34 +0900

    스크립트1 사용자 지정 클래스 serialization

--- Tukorea_GE_study1/Assets/Scripts/TU_Study.cs
+++ Tukorea_GE_study1/Assets/Scripts/TU_Study.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class complexType
+{
+    public string name;
+    public int age;
+    public float hieght;
+}
+
 public class TU_Study : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public complexType type; 
     void Start()
     {
     }
